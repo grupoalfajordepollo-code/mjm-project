cfc9716d80342c8b585ba14f735412faf8778ba2
Martin Olmedo <martineolmedo@outlook.com>
2026-08-23 01:24:55 -0300

    Finalizacion del maquetado del header y del footer

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -1,9 +1,13 @@
+import Footer from "./components/Footer"
+import Header from "./components/Header"
 import LoginForm from "./components/LoginForm"
 function App() {
 
   return (
     <>
+      <Header></Header>
       <LoginForm></LoginForm>
+      <Footer></Footer>
     </>
   )
 }
