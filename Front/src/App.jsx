f0f2f7d8ef63b046c872a580faf694c57ec7da2f
MauriDemasi <mauridemasi88@gmail.com>
2026-08-23 18:55:46 -0300

    Tst 1

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -2,6 +2,9 @@ import Categories from "./components/Categories"
 import Footer from "./components/Footer"
 import Header from "./components/Header"
 import Hero from "./components/Hero"
+import LoginForm from "./components/LoginForm"
+import RegisterForm from "./components/RegisterForm"
+import Unauthorized401 from "./components/Unauthorized401"
 
 function App() {
 
@@ -10,6 +13,8 @@ function App() {
       <Header></Header>
       <Hero></Hero>
       <Categories></Categories>
+      {/* <LoginForm></LoginForm> */}
+      {/* <RegisterForm></RegisterForm> */}
       <Footer></Footer>
     </>
   )
